912e97c623bc3f71d7ee4fa1f759e24fa6c375ce
Vic-Wele <91025736+Vic-Wele@users.noreply.github.com>
2022-09-09 22:33:04 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const prevBtn = document.getElementById("carousel-button-prev");
+const nextBtn = document.getElementById("carousel-button-next");
+const slides = document.getElementsByClassName("carousel-item");
+let slidePosition = 0;
+const totalSlides = slides.length;
+
+function hideAllSlides() {
+  for (let slide of slides) {
+    slide.classList.remove("carousel-item-visible");
+    slide.classList.add("carousel-item-hidden");
+  }
+}
+
+function moveToNextSlide() {
+  hideAllSlides();
+
+  if (slidePosition === totalSlides - 1) {
+    slidePosition = 0;
+  } else {
+    slidePosition++;
+  }
+
+  slides[slidePosition].classList.add("carousel-item-visible");
+}
+
+function moveToPrevSlide() {
+  hideAllSlides();
+
+  if (slidePosition === 0) {
+    slidePosition = totalSlides - 1;
+  } else {
+    slidePosition--;
+  }
+
+  slides[slidePosition].classList.add("carousel-item-visible");
+}
+
+prevBtn.addEventListener("click", moveToPrevSlide);
+
+nextBtn.addEventListener("click", moveToNextSlide);
+
+setInterval(() => {
+  moveToNextSlide();
+}, 10000);
